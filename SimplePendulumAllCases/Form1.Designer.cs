fb3c0abb73f9c25615a51ea4d22db7036c534cb6
Ibtasam Ahmad <85065814+Ibtasam-Ahmad@users.noreply.github.com>
2023-08-28 11:45:45 +0500

    Add files via upload

---
+++ SimplePendulumAllCases/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace SimplePendulumAllCases
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.simplePendulumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eulerFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eulerCromerFormToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dampedPendulumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eulerFormToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.eulerCromerFormToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.dampedDriverPendulumToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eulerFormToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.eulerCromerFormToolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.simplePendulumToolStripMenuItem,
+            this.dampedPendulumToolStripMenuItem,
+            this.dampedDriverPendulumToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(480, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // simplePendulumToolStripMenuItem
+            // 
+            this.simplePendulumToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eulerFormToolStripMenuItem,
+            this.eulerCromerFormToolStripMenuItem});
+            this.simplePendulumToolStripMenuItem.Name = "simplePendulumToolStripMenuItem";
+            this.simplePendulumToolStripMenuItem.Size = new System.Drawing.Size(113, 20);
+            this.simplePendulumToolStripMenuItem.Text = "Simple Pendulum";
+            // 
+            // eulerFormToolStripMenuItem
+            // 
+            this.eulerFormToolStripMenuItem.Name = "eulerFormToolStripMenuItem";
+            this.eulerFormToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.eulerFormToolStripMenuItem.Text = "Euler Form";
+            this.eulerFormToolStripMenuItem.Click += new System.EventHandler(this.eulerFormToolStripMenuItem_Click);
+            // 
+            // eulerCromerFormToolStripMenuItem
+            // 
+            this.eulerCromerFormToolStripMenuItem.Name = "eulerCromerFormToolStripMenuItem";
+            this.eulerCromerFormToolStripMenuItem.Size = new System.Drawing.Size(174, 22);
+            this.eulerCromerFormToolStripMenuItem.Text = "Euler Cromer Form";
+            this.eulerCromerFormToolStripMenuItem.Click += new System.EventHandler(this.eulerCromerFormToolStripMenuItem_Click);
+            // 
+            // dampedPendulumToolStripMenuItem
+            // 
+            this.dampedPendulumToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eulerFormToolStripMenuItem1,
+            this.eulerCromerFormToolStripMenuItem1});
+            this.dampedPendulumToolStripMenuItem.Name = "dampedPendulumToolStripMenuItem";
+            this.dampedPendulumToolStripMenuItem.Size = new System.Drawing.Size(122, 20);
+            this.dampedPendulumToolStripMenuItem.Text = "Damped Pendulum";
+            this.dampedPendulumToolStripMenuItem.Click += new System.EventHandler(this.dampedPendulumToolStripMenuItem_Click);
+            // 
+            // eulerFormToolStripMenuItem1
+            // 
+            this.eulerFormToolStripMenuItem1.Name = "eulerFormToolStripMenuItem1";
+            this.eulerFormToolStripMenuItem1.Size = new System.Drawing.Size(174, 22);
+            this.eulerFormToolStripMenuItem1.Text = "Euler Form";
+            this.eulerFormToolStripMenuItem1.Click += new System.EventHandler(this.eulerFormToolStripMenuItem1_Click);
+            // 
+            // eulerCromerFormToolStripMenuItem1
+            // 
+            this.eulerCromerFormToolStripMenuItem1.Name = "eulerCromerFormToolStripMenuItem1";
+            this.eulerCromerFormToolStripMenuItem1.Size = new System.Drawing.Size(174, 22);
+            this.eulerCromerFormToolStripMenuItem1.Text = "Euler Cromer Form";
+            this.eulerCromerFormToolStripMenuItem1.Click += new System.EventHandler(this.eulerCromerFormToolStripMenuItem1_Click);
+            // 
+            // dampedDriverPendulumToolStripMenuItem
+            // 
+            this.dampedDriverPendulumToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.eulerFormToolStripMenuItem2,
+            this.eulerCromerFormToolStripMenuItem2});
+            this.dampedDriverPendulumToolStripMenuItem.Name = "dampedDriverPendulumToolStripMenuItem";
+            this.dampedDriverPendulumToolStripMenuItem.Size = new System.Drawing.Size(156, 20);
+            this.dampedDriverPendulumToolStripMenuItem.Text = "Damped Driver Pendulum";
+            this.dampedDriverPendulumToolStripMenuItem.Click += new System.EventHandler(this.dampedDriverPendulumToolStripMenuItem_Click);
+            // 
+            // eulerFormToolStripMenuItem2
+            // 
+            this.eulerFormToolStripMenuItem2.Name = "eulerFormToolStripMenuItem2";
+            this.eulerFormToolStripMenuItem2.Size = new System.Drawing.Size(174, 22);
+            this.eulerFormToolStripMenuItem2.Text = "Euler Form";
+            this.eulerFormToolStripMenuItem2.Click += new System.EventHandler(this.eulerFormToolStripMenuItem2_Click);
+            // 
+            // eulerCromerFormToolStripMenuItem2
+            // 
+            this.eulerCromerFormToolStripMenuItem2.Name = "eulerCromerFormToolStripMenuItem2";
+            this.eulerCromerFormToolStripMenuItem2.Size = new System.Drawing.Size(174, 22);
+            this.eulerCromerFormToolStripMenuItem2.Text = "Euler Cromer Form";
+            this.eulerCromerFormToolStripMenuItem2.Click += new System.EventHandler(this.eulerCromerFormToolStripMenuItem2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "q";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 410);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem simplePendulumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eulerFormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eulerCromerFormToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dampedPendulumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eulerFormToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem eulerCromerFormToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem dampedDriverPendulumToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eulerFormToolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem eulerCromerFormToolStripMenuItem2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SimplePendulumAllCases/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimplePendulumAllCases
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dampedDriverPendulumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eulerFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int size = 300;
+            double[] w = new double[size];
+            double[] th = new double[size];
+            double[] t = new double[size];
+
+            w[0] = 0.1;
+            th[0] = 0.2;
+
+            double g = 9.8, l = 1, dt = 0.05;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                w[i+1] = w[i] - ( g / l ) * th[i] * dt;
+                th[i + 1] = th[i] + w[i] * dt;
+                t[i + 1] = t[i] + dt;
+            }
+
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Black);
+
+            for (int i = 0; i < size-1; i++)
+            {
+                gg.FillEllipse(sb, 50 + (float)t[i]*25, 250 - (float)th[i]*20, 5, 5);
+            }
+        }
+
+        private void eulerCromerFormToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int size = 300;
+            double[] w = new double[size];
+            double[] th = new double[size];
+            double[] t = new double[size];
+
+            w[0] = 0.1;
+            th[0] = 0.2;
+
+            double g = 9.8, l = 1, dt = 0.05;
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                w[i + 1] = w[i] - (g / l) * th[i] * dt;
+                th[i + 1] = th[i] + w[i+1] * dt;
+                t[i + 1] = t[i] + dt;
+            }
+
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Blue);
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 50 + (float)t[i] * 25, 250 - (float)th[i] * 30, 5, 5);
+            }
+        }
+
+        private void eulerFormToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            int size = 300;
+            double[] w = new double[size];
+            double[] th = new double[size];
+            double[] t = new double[size];
+
+            w[0] = 0.1;
+            th[0] = 0.2;
+
+            //double g = 9.8, l = 1, dt = 0.05, q = 1.5;
+            double g = 9.8, l = 1, dt = 0.05;
+            double q = double.Parse(textBox1.Text);
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                w[i + 1] = w[i] - (g / l) * th[i] * dt - q * w[i] * dt;
+                th[i + 1] = th[i] + w[i] * dt;
+                t[i + 1] = t[i] + dt;
+            }
+
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Black);
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 50 + (float)t[i] * 25, 250 - (float)th[i] * 150, 5, 5);
+            }
+        }
+
+        private void eulerCromerFormToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            int size = 300;
+            double[] w = new double[size];
+            double[] th = new double[size];
+            double[] t = new double[size];
+
+            w[0] = 0.1;
+            th[0] = 0.2;
+
+            //double g = 9.8, l = 1, dt = 0.05, q =10;
+            //double g = 9.8, l = 1, dt = 0.05, q = 1.5;
+            double g = 9.8, l = 1, dt = 0.05;
+            double q = double.Parse(textBox1.Text);
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                w[i + 1] = w[i] - (g / l) * th[i] * dt - q * w[i] * dt;
+                th[i + 1] = th[i] + w[i+1] * dt;
+                t[i + 1] = t[i] + dt;
+            }
+
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Blue);
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 50 + (float)t[i] * 25, 250 - (float)th[i] * 150, 5, 5);
+            }
+        }
+
+        private void eulerFormToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            int size = 300;
+            double[] w = new double[size];
+            double[] th = new double[size];
+            double[] t = new double[size];
+
+            w[0] = 0.1;
+            th[0] = 0.2;
+
+            //double g = 9.8, l = 1, dt = 0.05, q = 1.5, fd = 0, od = 2 ;
+            //double g = 9.8, l = 1, dt = 0.05, q = 1.5, fd = 0.5, od = 2;
+            //double g = 9.8, l = 1, dt = 0.05, q = 1.5, fd = 1.2, od = 2;
+            double g = 9.8, l = 1, dt = 0.05, fd = 1.2, od = 2;
+            double q = double.Parse(textBox1.Text);
+
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                w[i + 1] = w[i] - (g / l) * th[i] * dt - q * w[i] * dt + fd * Math.Sin(od * t[i]) * dt;
+                th[i + 1] = th[i] + w[i] * dt;
+                t[i + 1] = t[i] + dt;
+            }
+
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Black);
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 50 + (float)t[i] * 25, 250 - (float)th[i] * 150, 5, 5);
+            }
+        }
+
+        private void eulerCromerFormToolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            int size = 300;
+            double[] w = new double[size];
+            double[] th = new double[size];
+            double[] t = new double[size];
+
+            w[0] = 0.1;
+            th[0] = 0.2;
+
+            //double g = 9.8, l = 1, dt = 0.05, q = 1.5, fd = 0, od = 2 ;
+            //double g = 9.8, l = 1, dt = 0.05, q = 1.5, fd = 0.5, od = 2;
+            //double g = 9.8, l = 1, dt = 0.05, q = 1.5, fd = 1.2, od = 2;
+            double g = 9.8, l = 1, dt = 0.05, fd = 1.2, od = 2;
+            double q = double.Parse(textBox1.Text);
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                w[i + 1] = w[i] - (g / l) * th[i] * dt - q * w[i] * dt + fd * Math.Sin(od * t[i]) * dt;
+                th[i + 1] = th[i] + w[i+1] * dt;
+                t[i + 1] = t[i] + dt;
+            }
+
+            Graphics gg = CreateGraphics();
+            SolidBrush sb = new SolidBrush(Color.Blue);
+
+            for (int i = 0; i < size - 1; i++)
+            {
+                gg.FillEllipse(sb, 50 + (float)t[i] * 25, 250 - (float)th[i] * 150, 5, 5);
+            }
+        }
+
+        private void dampedPendulumToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
